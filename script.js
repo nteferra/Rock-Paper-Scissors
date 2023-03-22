6ed9384d99c50462ff154408f44e483ef410555e
nteferra <n.teferra@yahoo.com>
2023-03-22 16:25:42 -0400

    create function that simulates a game based on user selection

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ function getComputerChoice() {
 }
 
 
+function playRound() {
+    if (playerSelection === "Rock" && computerSelection === "Paper") {
+        return ("You lose! Paper beats Rock.")
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return ("You win! Rock beats Scissors.")
+    } else if (playerSelection === "Rock" && computerSelection === "Rock") {
+        return ("Draw! Try again.")
+    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+        return ("Draw! Try again")    
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return ("You win! Paper beats Rock.")    
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return ("You lose! Scissors beats paper.")    
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return ("You lose! Rock beats Scissors.")    
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return ("You win! Scissors beats Paper.")    
+    } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+        return ("Draw! Try again") 
+    } else  {
+        return "error" 
+    }
+}
+
+let playerSelection = "Scissors";
+let computerSelection = getComputerChoice();
+console.log(playRound())
