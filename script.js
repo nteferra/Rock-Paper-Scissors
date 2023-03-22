4d912937ae8b3012df25480e3062816d6517b9da
nteferra <n.teferra@yahoo.com>
2023-03-22 18:16:15 -0400

    update playRound function so that prompt is not hardcoded into function but instead stored in variables

--- script.js
+++ script.js
@@ -1,35 +1,55 @@
+// keep a tally of rounds won
+//first to five rounds wins
+//return to selection prompt after each round unless a victor emerges
+
 const choiceArray = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
     return choiceArray[Math.floor(Math.random() * choiceArray.length)];
 }
 
+let wins = 0;
+let losses = 0;
+
 
 function playRound() {
-    if (playerSelectionUC === "ROCK" && computerSelection === "Paper") {
-        return ("You lose! Paper beats Rock.")
-    } else if (playerSelectionUC === "ROCK" && computerSelection === "Scissors") {
-        return ("You win! Rock beats Scissors.")
-    } else if (playerSelectionUC === "ROCK" && computerSelection === "Rock") {
+    if (playerSelectionUC === "Rock" && computerSelection === "Paper") {
+        return lossPrompt
+        // return to prompt
+    } else if (playerSelectionUC === "Rock" && computerSelection === "Scissors") {
+        return winPrompt
+    } else if (playerSelectionUC === "Rock" && computerSelection === "Rock") {
         return ("Draw! Try again.")
-    } else if (playerSelectionUC === "PAPER" && computerSelection === "Paper") {
+    } else if (playerSelectionUC === "Paper" && computerSelection === "Paper") {
         return ("Draw! Try again")    
-    } else if (playerSelectionUC === "PAPER" && computerSelection === "Rock") {
-        return ("You win! Paper beats Rock.")    
-    } else if (playerSelectionUC === "PAPER" && computerSelection === "Scissors") {
-        return ("You lose! Scissors beats paper.")    
-    } else if (playerSelectionUC === "SCISSORS" && computerSelection === "Rock") {
-        return ("You lose! Rock beats Scissors.")    
-    } else if (playerSelectionUC === "SCISSORS" && computerSelection === "Paper") {
-        return ("You win! Scissors beats Paper.")    
-    } else if (playerSelectionUC === "SCISSORS" && computerSelection === "Scissors") {
+    } else if (playerSelectionUC === "Paper" && computerSelection === "Rock") {
+        return winPrompt    
+    } else if (playerSelectionUC === "Paper" && computerSelection === "Scissors") {
+        return lossPrompt   
+    } else if (playerSelectionUC === "Scissors" && computerSelection === "Rock") {
+        return lossPrompt   
+    } else if (playerSelectionUC === "Scissors" && computerSelection === "Paper") {
+        return winPrompt   
+    } else if (playerSelectionUC === "Scissors" && computerSelection === "Scissors") {
         return ("Draw! Try again") 
     } else  {
         return "error" 
     }
 }
 
-let playerSelection = "sCiSSors";
-let playerSelectionUC = playerSelection.toUpperCase();
+function game() {
+    console.log(playRound());
+    console.log(playRound());
+    console.log(playRound());
+    console.log(playRound());
+    console.log(playRound());
+}
+
+let playerSelection = prompt("Please choose your fighter");
+let playerSelectionUC = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
 let computerSelection = getComputerChoice();
+
+let winPrompt = "You win! " + playerSelectionUC + " beats " + computerSelection
+let lossPrompt = "You lose! " + computerSelection + " beats " + playerSelectionUC
+
 console.log(playRound())
